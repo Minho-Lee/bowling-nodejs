4dfd70e21a5e423b24256cb0edce3a13ccc18e73
Minho Lee <minho.lee.93@hotmail.com>
2017-07-01 17:43:10 -0400

    adding logo and starting to make chart.js at getplayers.html

--- public/js/main.js
+++ public/js/main.js
@@ -115,7 +115,6 @@ $("#getplayers").on('click', function() {
    $("#wrapper_div").fadeOut(500, function() {
       $('#load_main').load("getplayers.html", function() {
          $("#wrapper_div").fadeIn(500);
-         
          /*$("#getInfo").on('click', function() {
             $("#displayInfo").DataTable({
                "paging": false,
@@ -186,6 +185,44 @@ $("#getplayers").on('click', function() {
                      counter++;
                   };
                   $("#results").append('<br/>');
+                  //making chart
+                  var ctx = document.getElementById("myChart").getContext('2d');
+                  var myChart = new Chart(ctx, {
+                      type: 'bar',
+                      data: {
+                          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                          datasets: [{
+                              label: '# of Votes',
+                              data: [12, 19, 3, 5, 2, 3],
+                              backgroundColor: [
+                                  'rgba(255, 99, 132, 0.2)',
+                                  'rgba(54, 162, 235, 0.2)',
+                                  'rgba(255, 206, 86, 0.2)',
+                                  'rgba(75, 192, 192, 0.2)',
+                                  'rgba(153, 102, 255, 0.2)',
+                                  'rgba(255, 159, 64, 0.2)'
+                              ],
+                              borderColor: [
+                                  'rgba(255,99,132,1)',
+                                  'rgba(54, 162, 235, 1)',
+                                  'rgba(255, 206, 86, 1)',
+                                  'rgba(75, 192, 192, 1)',
+                                  'rgba(153, 102, 255, 1)',
+                                  'rgba(255, 159, 64, 1)'
+                              ],
+                              borderWidth: 1
+                          }]
+                      },
+                      options: {
+                          scales: {
+                              yAxes: [{
+                                  ticks: {
+                                      beginAtZero:true
+                                  }
+                              }]
+                          }
+                      }
+                  });//myChart
                },
                error: function(xhr, textStatus, error){
                   console.log(xhr.statusText);
@@ -194,7 +231,8 @@ $("#getplayers").on('click', function() {
                }
             }); //ajax done
             console.log("post action completed");
-         });
+            
+         });//getInfo
       });//load_main
    });//wrapper_div
 });//getplayers
